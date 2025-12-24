7d43bea874a87bc936a8b37276ede3751ca65c2a
Caio Monte <caiomm076@gmail.com>
2025-12-23 22:05:50 -0300

    correção de diretório

--- automacoes/automacao_de_curadoria_de_conteudo/feed_reader.py
+++
@@ -1,9 +0,0 @@
-import feedparser
-from config import FEEDS
-
-def fetch_news():
-    news_data = {}
-    for site, url in FEEDS.items():
-        parsed_feed = feedparser.parse(url)
-        news_data[site] = parsed_feed.entries
-    return news_data
